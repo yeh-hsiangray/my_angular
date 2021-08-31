2a893a9eb8a976a4664dc3da75a9cd5c88401ff5
Jack.Yeh <jackyehh5225@gmail.com>
2021-08-31 17:53:12 +0900

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div style="width: 800px;margin: auto;">
+  template: `<div class="container">
+  <div class="row">
+  <div class="col">
+  </div>
+  <div class="col-10">
   <h1 class="display-1" style="text-align:center;">{{title}}</h1>
   <calendar></calendar>
-  </div>`
+  </div>
+  <div class="col">
+  </div>
+  </div>
+  </div>
+  `
+  
 })
 export class AppComponent {
   title:string = '單日行事曆';
